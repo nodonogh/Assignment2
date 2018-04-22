9ec86d7155d71eb14536bdd652f752ec2cfc1707
user138925 <user138925@74-80-245-59.ic2net.net>
2018-04-21 21:07:47 -0700

    First check in for Assignment 2

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by user138925 on 4/21/18.
+//  Copyright © 2018 user138925. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
